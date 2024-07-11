a6bd328dbbeb402595d5760c0f7ee0e726c69534
JULLACE <itsaruckaz@gmail.com>
2024-07-11 02:13:39 -0400

    Added automatic authentication using riot_auth library

---
+++ src/auth.py
@@ -0,0 +1,23 @@
+import riot_auth, asyncio
+import os, dotenv, sys
+
+import requests
+
+async def load_cred():
+    dotenv.load_dotenv()
+
+    current_version = requests.get("https://valorant-api.com/v1/version").json()["data"]["riotClientBuild"]
+
+    from riot_auth import RiotAuth
+    RiotAuth.RIOT_CLIENT_USER_AGENT = f"RiotClient/{current_version} rso-auth (Windows;10;;Professional, x64)"
+    CREDS = os.getenv('USER'), os.getenv('PASS')
+
+    auth = riot_auth.RiotAuth()
+    await auth.authorize(*CREDS)
+
+    # print(f"Access Token Type: {auth.token_type}\n")
+    # print(f"Access Token: {auth.access_token}\n")
+    # print(f"Entitlements Token: {auth.entitlements_token}\n")
+    # print(f"User ID: {auth.user_id}")
+
+    return auth
---
+++ src/run.py
@@ -0,0 +1,71 @@
+import discord, aiohttp
+import os, dotenv
+
+import auth
+
+dotenv.load_dotenv()
+bot = discord.Bot()
+
+CURRENT_SEASON = '52ca6698-41c1-e7de-4008-8994d2221209'
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} up and ready!")
+    tokens = await auth.load_cred()
+    if tokens != None:
+        bot.HEADERS = {
+            "X-Riot-Entitlements-JWT": f"{tokens.entitlements_token}",
+            "X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9",
+            "X-Riot-ClientVersion": "release-09.00-shipping-28-2628993",
+            "Authorization": f"{tokens.token_type} {tokens.access_token}",
+        }
+
+
+@bot.slash_command(name="hello", description="Make sure bot is breathing")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hi :)")
+
+@bot.slash_command(name="register", description="Register a player for the bot to track")
+async def register(ctx: discord.ApplicationContext, player: str):
+    ctx.respond("WIP")
+
+@bot.slash_command(name="check", description="Check out a player's stats")
+async def check(ctx: discord.ApplicationContext, player: str):
+    embed = discord.Embed(
+        title="Minos",
+        color=discord.Color.dark_blue()
+    )
+
+    embed.set_footer(text="If you see this, hi") # footers can have icons too
+    embed.set_author(name="Player's Info", icon_url="https://pbs.twimg.com/media/Fbb1PYFagAA6579.png")
+    embed.set_thumbnail(url="https://static.wikia.nocookie.net/ficcion-sin-limites/images/d/d8/Minos_Phrime.png/revision/latest/scale-to-width-down/600?cb=20230906001709&path-prefix=es")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://pd.na.a.pvp.net/mmr/v1/players/18f595d7-ec04-513c-bcf5-53d6914f9a53', headers=bot.HEADERS) as r:
+            if r.status == 200:
+                data = await r.json(content_type=None)
+                formatted = data['QueueSkills']['competitive']['SeasonalInfoBySeasonID'][CURRENT_SEASON]
+                formatted = clean_info_season(formatted)
+
+                embed.add_field(name="Current Season Info", value=formatted, inline=True)
+                
+                await ctx.respond(embed=embed)
+            else:
+                await ctx.respond("i require assistance...")
+                print(r.text)
+                print(r.json(content_type=None))
+
+def clean_info_season(unf):
+    wins = unf['NumberOfWins']
+    games = unf['NumberOfGames']
+    wr = round(((float(wins) / float(games)) * 100), 1)
+
+    res = f"""**Wins: {wins}**
+              Games: {games}
+              Winrate: {wr}%
+              Rank: {unf['Rank']}"""
+    
+    return res
+
+
+bot.run(os.getenv('TOKEN'))
